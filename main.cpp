56f7fc5ca167158ee98b9a4a5975481d0b876c97
Hua-Shan Wu <greenballow@gmail.com>
2020-04-01 18:07:40 +0800

    A

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 PwmOut PWM1(D6);
+Serial pc( USBTX, USBRX );
+AnalogIn Ain(A0);
+float ADCdata;
 
 int main()
 {
-    //Q1
+    //Q2
     // basic printf demo = 16 by 18 characters on screen
     uLCD.printf("\nStarting Demo...\n"); //Default Green on black text
     uLCD.printf("\n  108038520\n");
@@ -15,7 +18,7 @@ int main()
     uLCD.color(RED);
     uLCD.filled_rectangle(50, 50, 100, 100, 0xFFFFFF);
 
-    //Q2
+    //Q3
     for (int i = 0; i<9; i++){
       PWM1.period(0.001);
       PWM1 = 0.1+0.1*i;
@@ -24,6 +27,11 @@ int main()
       PWM1.period(0.001);
       PWM1 = 1.0-0.1*i; 
     }
+
+    //Q4
+    ADCdata = Ain;
+    wait(0.1);
+    pc.printf("%1.3f\r\n", ADCdata);
 
 }
 
